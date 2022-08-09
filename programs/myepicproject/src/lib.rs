54d7d7cb33fd5bd8f4c8ebabe53cd51c9daddd1a
priks <gautamprikshit1@gmail.com>
2022-08-09 20:21:46 +0530

    Initial commit

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -18,10 +18,21 @@ pub mod myepicproject {
         let item = ItemStruct {
             gif_link: gif_link.to_string(),
             user_address: *user.to_account_info().key,
+            votes: 0,
         };
         base_account.gif_list.push(item);
         base_account.total_gifs += 1;
         Ok(())
+    }
+
+    pub fn update_item(ctx: Context<UpdateItem>, item_url: String) -> Result<()> {
+        let base_account = &mut ctx.accounts.base_account;
+        for item in base_account.gif_list.iter_mut() {
+            if item.gif_link == item_url {
+                item.votes += 1;
+            }
+        }
+        Ok(()) 
     }
 }
 
@@ -42,10 +53,17 @@ pub struct AddGif<'info> {
     pub user: Signer<'info>,
 }
 
+#[derive(Accounts)]
+pub struct UpdateItem<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
 #[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
 pub struct ItemStruct {
     pub gif_link: String,
     pub user_address: Pubkey,
+    pub votes: u64,
 }
 
 #[account]
